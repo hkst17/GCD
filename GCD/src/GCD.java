7482694520ae1ba545fdbf537ecaec056a4f1c34
sandratiu <sandratiu@10.0.1.9>
2017-01-30 15:18:34 -0800

    Ini Commit

---
+++ GCD/src/GCD.java
@@ -0,0 +1,14 @@
+/**
+ * Name:        Sandra Latt
+ * Cruz ID:     slatt
+ * Assignment:  GCD
+ * Description:  /desc?/
+ * Date:        3:11:01 PM Jan 30, 2017
+ */
+public class GCD {
+
+	public static void main(String[] args) {
+		//test
+	}
+
+}

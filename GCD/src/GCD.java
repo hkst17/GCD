0bc5791eacbdb69581c83805253144a122909d3f
sandratiu <sandratiu@10.0.1.9>
2017-01-30 15:23:06 -0800

    Second Commit Import old stuff

--- GCD/src/GCD.java
+++ GCD/src/GCD.java
@@ -7,8 +7,33 @@
  */
 public class GCD {
 
-	public static void main(String[] args) {
-		//test
-	}
+	public static void main( String[] args ){
+
+	      Scanner sc = new Scanner(System.in);
+
+	      System.out.print("Enter a positive integer: ");
+	      int x = sc.nextInt();
+	         if (x < 0){
+	            System.out.print("Please enter a positive integer: ");
+	          
+	         }
+	      System.out.print("Enter another positive integer: ");
+	      int y = sc.nextInt();
+
+	      System.out.print("The GCD of ");
+	      System.out.print(x);
+	      System.out.print(" and ");
+	      System.out.print(y);
+	      System.out.print(" is ");
+	      System.out.print(+ gcd(x, y));      
+	    }
+
+	   private static int gcd(int x, int y){
+	      if(y == 0){
+	         return x;
+	      }
+	      return gcd(y, x%y);
+	   }
+
 
 }
